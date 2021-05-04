d82ff9cf69571288df767e1dd81ef40e67af00e6
gr221004 <gr221004@graded.br>
2021-05-04 13:54:39 -0300

    fala

--- index.js
+++ index.js
@@ -12,14 +12,41 @@ var triangles = [];
 
 function preload() {
   //load in images from the data folder
+  for (let i = 0; i < 30; i++) {
+    let index = nf(i + 1, 4, 0);
+    circles.push(loadImage("data/circle" + index + ".png"));
+    squares.push(loadImage("data/square" + index + ".png"));
+    triangles.push(loadImage("data/triangle" + index + ".png"));
+  };
 
 }
+
+let shapeClassifier;
 
 function setup() {
   createCanvas(500, 500);
 
+  let options = {
+    inputs: [100, 100, 4],
+    task: "imageClassification",
+    debug: true,
+  };
+
+  shapeClassifier = ml5.neuralNetwork(options);
+
+  for (let i = 0; i < circles.length; i++) {
+    shapeClassifier.addData({image: circles[i]}, {label: "circle"});
+    shapeClassifier.addData({image: triangles[i]}, {label: "triangle"});
+    shapeClassifier.addData({image: squares[i]}, {label: "square"});
+  };
+  shapeClassifier.normalizeData();
+  shapeClassifier.train({epochs: 50}, finishedTraining());
+
 }
 
+function finishedTraining() {
+  text("Finished Training!", 0, 0);
+}
 
 function draw() {
 
